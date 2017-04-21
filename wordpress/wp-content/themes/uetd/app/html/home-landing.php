9cf144ae93dfc545968ad7e202cbdb355bc3091a
Madlax <yanistam@hotmail.fr>
2017-04-21 12:44:00 +0200

    Gros fix mamène

--- wordpress/wp-content/themes/uetd/app/html/home-landing.php
+++ wordpress/wp-content/themes/uetd/app/html/home-landing.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<section class="landing">
+<section class="landing" id="landing">
     <div class="landing__background">
         <video class="landing__video" loop muted autoplay poster="http://u-d.ivanprunier.fr/wp-content/uploads/2017/04/img-principale-1.jpg" class="fullscreen-bg__video">
             <source src="http://u-d.ivanprunier.fr/wp-content/uploads/2017/04/EF_video-home_v2_web.webm" type="video/webm">
@@ -10,16 +9,9 @@
                 <p class="landing__subtitle"><?php the_field("landing_subtitle"); ?></p>
             </div>
             <a href="#contact"><button class="landing__contact">Nous contacter</button></a>
-            <button class="landing__button">
+            <a href="#about" class="landing__button">
                 <img class="landing__arrow" src="http://u-d.ivanprunier.fr/wp-content/uploads/2017/04/arrow-angle.png">
-            </button>
-=======
-<section class="landing" id="landing">
-    <div class="landing__background" style="background-image:url('http://u-d.ivanprunier.fr/wp-content/uploads/2017/04/img-principale-1.jpg">
-        <div class="landing__content">
-            <h1 class="landing__title"><?php the_field("landing_title"); ?></h1>
-            <p class="landing__subtitle"><?php the_field("landing_subtitle"); ?></p>
->>>>>>> d206ad1a992a18c647119c72a22af1cfee9f6753
+            </a>
         </div>
     </div>
 </section>
